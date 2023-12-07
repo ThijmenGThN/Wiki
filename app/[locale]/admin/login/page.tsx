a617b96862369509b584a1a8564ad1ad54841cc0
ThijmenGThN <mail@thijmenheuvelink.nl>
2023-12-07 20:58:51 +0000

    🔒️ Improved login

--- app/[locale]/admin/login/page.tsx
+++ app/[locale]/admin/login/page.tsx
@@ -1,43 +1,37 @@
 "use client"
 
-import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 
 import { Link } from '@/helpers/navigation'
 
-import OAuth from '../OAuth'
 import Credentials from './Credentials'
 
-import aLogo from '@/assets/logo.webp'
-
 export default function Page() {
-    const t = useTranslations('auth')
+    const t = useTranslations()
 
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <Link href="/">
-                    <Image
-                        className="mx-auto h-10 w-auto"
-                        src={aLogo}
-                        alt=""
-                    />
-                </Link>
-                <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    {t('sign-in-to-your-account')}
-                </h2>
+            <div className="flex flex-col gap-y-3 text-center">
+                <div className='flex justify-center'>
+                    <Link href="/">
+                        <p className="text-2xl px-4 font-semibold hover:cursor-pointer">
+                            {t('branding.title')}
+                        </p>
+                    </Link>
+                </div>
+                <p className="text-sm">
+                    {t('branding.description')}
+                </p>
             </div>
 
             <div className="relative my-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
                 <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
                     <Credentials />
-
-                    <OAuth />
                 </div>
 
                 <div className="absolute -bottom-10 left-5 text-center text-sm text-gray-500">
                     <Link href="/">
-                        ← {t('return-to-wiki')}
+                        ← {t('auth.return-to-wiki')}
                     </Link>
                 </div>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
+import { useTranslations } from 'next-intl'
 
 import { Link } from '@/helpers/navigation'
 
 export default function Header() {
+    const t = useTranslations()
+
     return (
         <div className="flex flex-col gap-y-3 text-center">
             <div className='flex justify-center'>
                 <Link href="/">
                     <p className="text-2xl px-4 font-semibold hover:cursor-pointer">
-                        Wiki
+                        {t('branding.title')}
                     </p>
                 </Link>
             </div>
             <p className="text-sm">
-                Maintained by Thijmen Heuvelink
+                {t('branding.description')}
             </p>
         </div>
     )
